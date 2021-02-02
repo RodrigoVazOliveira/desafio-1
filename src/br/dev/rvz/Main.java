0b6147c6ea7bc8853e2b7447748589f9be52211f
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-02 13:21:49 -0300

    feito menu de entrada

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -11,6 +11,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int pontuacao = 0, numeroAleatorio = gerarNumeroAleatorio(10), entradaJogador;
+
+        System.out.println("Bem vindo ao jogo do adivinha um número");
+        System.out.println("Por faor digite um número: ");
+        entradaJogador = Integer.parseInt(in.nextLine());
+
+        if ()
+
     }
 }
