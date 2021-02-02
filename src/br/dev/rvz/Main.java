d1f64565e5667a602065cd3ce2f6df05f7108747
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-02 13:41:40 -0300

    Entrega média

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -10,18 +10,41 @@ public class Main {
         return random.nextInt(tamanho);
     }
 
+    public static int testeDeAcerto(int numeroJogador, int numeroAleatorio) {
+        if (numeroJogador == numeroAleatorio) {
+            System.out.println("Parabéns, você ganhou mais 10 pontos");
+            return 10;
+        } else if (numeroJogador == (numeroAleatorio+1) || numeroJogador == (numeroAleatorio-1)) {
+            System.out.println("Parabéns, você ganhou mais 5 pontos");
+            return 5;
+        } else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-        int pontuacao = 0, numeroAleatorio = gerarNumeroAleatorio(10), entradaJogador;
+        int pontuacao = 0, numeroAleatorio = gerarNumeroAleatorio(5), entradaJogador;
+        boolean continuar = true;
+        String tentarNovamente;
 
         System.out.println("Bem vindo ao jogo do adivinha um número");
-        System.out.println("Por faor digite um número: ");
-        entradaJogador = Integer.parseInt(in.nextLine());
 
-        if (entradaJogador == numeroAleatorio) {
-            pontuacao += 10;
-        } else if (entradaJogador == (numeroAleatorio+1) || entradaJogador == (numeroAleatorio-1)) {
-            pontuacao += 5;
+        while(continuar) {
+
+            System.out.println("Por faor digite um número: ");
+            entradaJogador = Integer.parseInt(in.nextLine());
+
+            pontuacao += testeDeAcerto(entradaJogador, numeroAleatorio);
+
+            System.out.println("Digite \"S\" para continuar e \"n\" para sair do jogo");
+            tentarNovamente = in.nextLine();
+
+            if (tentarNovamente.equalsIgnoreCase("n")) {
+                continuar = false;
+            }
+
         }
 
         System.out.println("A sua pontuação final é: " + pontuacao);
