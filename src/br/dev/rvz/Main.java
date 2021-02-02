5056e4dbc0d331575d0f0ecb1aacb9511286fd24
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-02 13:15:46 -0300

    feito o gerador número aletorio

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -1,8 +1,16 @@
 package br.dev.rvz;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
+    public static int gerarNumeroAleatorio(int tamanho) {
+        Random random = new Random();
+        return random.nextInt(tamanho);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        
     }
 }
